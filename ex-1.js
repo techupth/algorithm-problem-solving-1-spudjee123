66eae4faf7a0e6c2a54cc0d006eaf8fa21524938
unknown <sornram.spy@gmail.com>
2024-07-15 18:48:26 +0700

    feat:spy

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,20 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
+
+    if (sum === target) {
+      return [left, right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 18));
